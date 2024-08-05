3ade4c390e0eb71dbe34b8e4961699eeb44e69d6
Admin <12376532+baiyuqi_1_0@user.noreply.gitee.com>
2024-08-06 07:51:08 +0800

    cluster

--- asociety/evaluation/age_simularity.py
+++ asociety/evaluation/age_simularity.py
@@ -1,24 +1,40 @@
 import pandas as pd
 
-def real_mean_age():
-    mdf = pd.read_csv('data/cross_section/age_mean.csv')
-    s_array = mdf[mdf.columns[2:4]].to_numpy()
-    import numpy as np
-    pc = np.corrcoef(s_array)
-    print(pc)
-def real_mean_variation():
-    vdf = pd.read_csv('data/cross_section/age_variation.csv')
+def real_mean_age(section):
+    mdf = pd.read_csv('data/cross_section/age_mean_' + section + '.csv')
+    return mdf
+    #s_array = mdf[mdf.columns[2:4]].to_numpy()
+    # import numpy as np
+    # pc = np.corrcoef(s_array)
+    # print(pc)
+def real_mean_variation(section):
+    vdf = pd.read_csv('data/cross_section/age_variation_' + section + '.csv')
+def align(real, llm):
+    rrange = set(real['age_range'].to_numpy())
+
+    print(rrange)
+    lrange = set(llm['age_range'].to_numpy())
+    inter = rrange.intersection(lrange)
+    real = real.loc[real['age_range'].isin(inter)]
+    llm = llm.loc[llm['age_range'].isin(inter)]
+    return (real, llm)
 
 def llm_mean_age():
-    sql = '''select age_range,count(*),AVG(extraversion),AVG(agreeableness),AVG(conscientiousness),AVG(neuroticism),AVG(openness)  from persona_personality GROUP BY age_range '''
+    sql = '''select age_range,count(*) as sample_size,AVG(extraversion),AVG(agreeableness),AVG(conscientiousness),AVG(neuroticism),AVG(openness)  from persona_personality GROUP BY age_range '''
+
+   
     from asociety.repository.database import engine
-    from sqlalchemy import text
-    from sqlalchemy.orm import Session
-    with Session(engine) as session:
-        ps = session.execute(text(sql))
-        for i, r in enumerate(ps):
-            tupe = r.tuple()
-            tupe = list(tupe[2:])
-            pass
+    df = pd.read_sql(sql, engine)
+    return df
+    # from sqlalchemy import text
+    # from sqlalchemy.orm import Session
+    # with Session(engine) as session:
+    #     ps = session.execute(text(sql))
+    #     for i, r in enumerate(ps):
+    #         tupe = r.tuple()
+    #         tupe = list(tupe[2:])
+    #         pass
 if __name__ == "__main__": 
-    llm_mean_age()
+    llm = llm_mean_age()
+    real = real_mean_age('BHPS')
+    align(real, llm)
