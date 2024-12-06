2fd8059ac5cc947f9cd753fc2ebe2cbf1a74dea9
Admin <12376532+baiyuqi_1_0@user.noreply.gitee.com>
2024-12-06 16:24:46 +0800

    ccc

--- asociety/generator/llm_engine_test.py
+++ asociety/generator/llm_engine_test.py
@@ -12,14 +12,14 @@ messages = [
     )
 ]
 
-from langchain_openai import OpenAI
-llm = OpenAI(
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI(
         temperature = 0.7,
         model="/data1/glm-4-9b-chat",
         openai_api_key="aaa",
-        openai_api_base="http://221.229.101.198:8000/v1",
-        model_kwargs={"stop": ["<|user|>", "<|observation|>", '<|endoftext|>']},
+        openai_api_base="http://221.229.101.198:8000/v1"
+ 
     )
 
 answer = llm.invoke(messages)
-print(answer)
+print(answer.content)
